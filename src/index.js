126fcf6dfe1788e4427996b466064e065d0835ec
Leon Ho <fupuchu@gmail.com>
2018-12-05 11:10:00 +0800

    USE THIS COMMIT AND BRANCH

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import nav from "./nav";
+// import nav from "./nav";
 import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 import makeImage from "./image";
 import imageUrl from "./webpack-logo.jpg";
-import css from "./footer.css";
-import buttonStyles from "./button.css";
+// import css from "./footer.css";
+// import buttonStyles from "./button.css";
 
 const image = makeImage(imageUrl);
 const button = makeButton("Yay! A Button!");
